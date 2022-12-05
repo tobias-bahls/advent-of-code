c573660e350f62592944e32ee2cfef466760cea6
Tobias Bahls <tobias.bahls@gmail.com>
2022-12-05 08:57:06 +0100

    Day 5

---
+++ src/main/kotlin/Day05.kt
@@ -0,0 +1,92 @@
+import Part.PART1
+import Part.PART2
+
+@JvmInline value class Crate(val char: Char)
+
+data class Crates(val stacks: Map<Int, List<Crate>>) {
+    companion object {
+        fun parse(input: String): Crates {
+            val lines = input.lines()
+            val stackIndexes = lines.last().split(" ").filterNotBlank().mapInts()
+
+            val stacks =
+                stackIndexes.associateWith { i ->
+                    val column = ((i - 1) * 4) + 1
+
+                    val stack =
+                        lines
+                            .dropLast(1)
+                            .map { it.getOrNull(column) ?: ' ' }
+                            .filterWhitespace()
+                            .map { Crate(it) }
+
+                    stack.reversed().toMutableList()
+                }
+
+            return Crates(stacks)
+        }
+    }
+
+    fun executeInstruction(instruction: Instruction, mode: Part): Crates {
+        val fromStack = stacks.getValue(instruction.from)
+        val toStack = stacks.getValue(instruction.to)
+
+        val moved =
+            when (mode) {
+                PART1 -> fromStack.reversed().take(instruction.count)
+                PART2 -> fromStack.takeLast(instruction.count)
+            }
+
+        val newTo = toStack + moved
+        val newFrom = fromStack.dropLast(instruction.count)
+
+        val newStacks =
+            stacks.mapValues { (idx, stack) ->
+                when (idx) {
+                    instruction.from -> newFrom
+                    instruction.to -> newTo
+                    else -> stack
+                }
+            }
+
+        return Crates(newStacks)
+    }
+}
+
+data class Instruction(val count: Int, val from: Int, val to: Int) {
+    companion object {
+        private val REGEX = """move (\d+) from (\d+) to (\d+)""".toRegex()
+
+        fun parse(input: String): Instruction {
+            val (count, from, to) = REGEX.find(input)!!.destructured
+
+            return Instruction(count.toInt(), from.toInt(), to.toInt())
+        }
+    }
+}
+
+fun main() {
+    val input = readResourceAsString("day05.txt")
+
+    val (rawStack, rawInstructions) = input.twoParts("\n\n").toPair()
+    val crates = Crates.parse(rawStack)
+    val instructions = rawInstructions.parseLines { Instruction.parse(it) }
+
+    part1 {
+        instructions
+            .fold(crates) { acc, instruction -> acc.executeInstruction(instruction, PART1) }
+            .stacks
+            .values
+            .map { it.last().char }
+            .joinToString("")
+    }
+
+    part2 {
+        instructions
+            .fold(crates) { acc, instruction -> acc.executeInstruction(instruction, PART2) }
+            .stacks
+            .values
+            .map { it.last().char }
+            .joinToString("")
+    }
+}
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,6 +1,12 @@
-fun List<String>.filterNotBlank() = this.filter { it.isNotBlank() }
+fun List<String>.filterNotBlank() = this.map { it.trim() }.filter { it.isNotBlank() }
+
+fun List<Char>.filterWhitespace() = this.filter { !it.isWhitespace() }
 
 fun List<String>.mapInts() = this.map { it.toInt() }
+
+fun <T> List<T>.toPair(): Pair<T, T> = this.expectSize(2).let { (a, b) -> Pair(a, b) }
+
+fun String.twoParts(char: String): List<String> = this.split(char).expectSize(2)
 
 fun String.twoParts(char: Char): List<String> = this.split(char).expectSize(2)
 
@@ -14,19 +20,26 @@ fun <T> List<T>.expectSize(expectedSize: Int): List<T> {
 
 fun <T> T.dump(): T = this.also { println(this) }
 
+fun <T> T.dump(prefix: String): T = this.also { println("$prefix: $this") }
+
 fun <T> List<T>.sample(): List<T> = this.subList(0, 3).dump()
 
 fun readResourceAsString(name: String): String =
-    object {}::class.java.getResource(name)!!.readText()
+    object {}::class.java.getResource(name)?.readText() ?: error("Did not find file $name")
 
 fun <T> String.parseLines(parser: (String) -> T): List<T> =
     this.lines().filterNotBlank().map(parser)
 
 fun <T> solve(msg: String, block: () -> T) {
     val result = block()
-    println("$msg: $result")
+    if (result != Unit) println("$msg: $result")
 }
 
 fun <T> part1(block: () -> T) = solve("Part 1", block)
 
 fun <T> part2(block: () -> T) = solve("Part 2", block)
+
+enum class Part {
+    PART1,
+    PART2
+}

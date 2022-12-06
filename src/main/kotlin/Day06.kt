44220f44fd429bfc3bdec25cbb03a93a7fc90eb8
Tobias Bahls <tobias.bahls@gmail.com>
2022-12-06 08:22:25 +0100

    Day 06

---
+++ src/main/kotlin/Day06.kt
@@ -0,0 +1,16 @@
+fun solve(input: String, markerLength: Int): Int {
+    val markerIndex =
+        input.windowed(markerLength, 1).indexOfFirst {
+            it.toCharArray().distinct().size == markerLength
+        }
+
+    return markerIndex + markerLength
+}
+
+fun main() {
+    val input = readResourceAsString("day06.txt")
+
+    part1 { solve(input, 4) }
+
+    part2 { solve(input, 14) }
+}

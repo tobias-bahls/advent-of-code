03cc494993ea3aca1e8b361f2f2847ec4425f642
Tobias Bahls <tobias.bahls@gmail.com>
2022-12-08 08:37:12 +0100

    2022: Day 08

---
+++ aoc2022/src/main/kotlin/Day08.kt
@@ -0,0 +1,55 @@
+import datastructures.Grid
+import datastructures.Tile
+import utils.part1
+import utils.part2
+import utils.readResourceAsString
+import utils.visitAllNodes
+
+fun walkTiles(tile: Tile<Int>, block: (Tile<Int>) -> Tile<Int>?) =
+    visitAllNodes(listOf(tile)) { t -> block(t)?.let { listOf(it) } ?: listOf() }
+
+fun tilesAlong(tile: Tile<Int>, block: (Tile<Int>) -> Tile<Int>?) =
+    walkTiles(tile) { block(it) }.drop(1)
+
+fun visibleAlong(tile: Tile<Int>, block: (Tile<Int>) -> Tile<Int>?) =
+    tilesAlong(tile, block).all { it.data < tile.data }
+
+fun viewingDistance(tile: Tile<Int>, block: (Tile<Int>) -> Tile<Int>?): Int {
+    val tiles = tilesAlong(tile, block)
+    val blockingTreeIdx = tiles.indexOfFirst { it.data >= tile.data }
+
+    return if (blockingTreeIdx == -1) {
+        tiles.size
+    } else {
+        blockingTreeIdx + 1
+    }
+}
+
+fun main() {
+    val input = readResourceAsString("/day08.txt")
+    val grid = Grid(input) { it.digitToInt() }
+
+    part1 {
+        grid.tiles
+            .filter { tile ->
+                when {
+                    tile.isEdge() -> true
+                    visibleAlong(tile) { it.above } -> true
+                    visibleAlong(tile) { it.right } -> true
+                    visibleAlong(tile) { it.below } -> true
+                    visibleAlong(tile) { it.left } -> true
+                    else -> false
+                }
+            }
+            .size
+    }
+
+    part2 {
+        grid.tiles.maxOf { tile ->
+            viewingDistance(tile) { it.above } *
+                viewingDistance(tile) { it.right } *
+                viewingDistance(tile) { it.below } *
+                viewingDistance(tile) { it.left }
+        }
+    }
+}
--- utils/src/main/kotlin/datastructures/Grid.kt
+++ utils/src/main/kotlin/datastructures/Grid.kt
@@ -6,9 +6,33 @@ import utils.match
 import utils.toPair
 
 data class Tile<T>(val grid: Grid<T>, val point: Point, val data: T) {
+    val above: Tile<T>?
+        get() = grid.tileAt(point.top)
+
+    val right: Tile<T>?
+        get() = grid.tileAt(point.right)
+
+    val below: Tile<T>?
+        get() = grid.tileAt(point.bottom)
+
+    val left: Tile<T>?
+        get() = grid.tileAt(point.left)
+
     fun adjacentOrthogonally(): List<Tile<T>> =
         point.neighboursOrthogonally.mapNotNull { grid.tileAt(it) }
     fun adjacent(): List<Tile<T>> = point.neighbours.mapNotNull { grid.tileAt(it) }
+
+    fun isEdge(): Boolean {
+        if (point.x == 0 || point.y == 0) {
+            return true
+        }
+
+        if (point.x == grid.width || point.y == grid.height) {
+            return true
+        }
+
+        return false
+    }
 
     override fun toString(): String {
         return "Tile(point=$point, data=$data)"
@@ -46,6 +70,8 @@ fun parsePoint(input: String) =
 
 class Grid<T>(input: String, createTile: (Char) -> T) {
     val tiles: List<Tile<T>>
+    val width: Int
+    val height: Int
 
     init {
         val tiles =
@@ -56,6 +82,8 @@ class Grid<T>(input: String, createTile: (Char) -> T) {
             }
 
         this.tiles = tiles
+        this.width = tiles.maxOf { it.point.x }
+        this.height = tiles.maxOf { it.point.y }
     }
 
     fun tileAt(point: Point) = tiles.firstOrNull { it.point == point }

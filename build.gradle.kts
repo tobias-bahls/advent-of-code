80b81a4eca100aa2bb06d011086a585c391e9d94
Tobias Bahls <tobias.bahls@gmail.com>
2022-12-12 19:38:07 +0100

    Add version upgrades

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 import com.diffplug.gradle.spotless.SpotlessExtension
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.21" apply false
+    kotlin("jvm") version "1.7.22" apply false
     id("com.diffplug.spotless") version "6.12.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18" apply false
+    id("com.github.ben-manes.versions") version "0.44.0" apply false
 }
 
 configure<SpotlessExtension> { kotlinGradle { ktfmt().dropboxStyle() } }
@@ -13,8 +16,18 @@ allprojects {
     version = "NONE"
 
     apply(plugin = "com.diffplug.spotless")
-
+    apply(plugin = "se.patrikerdes.use-latest-versions")
+    apply(plugin = "com.github.ben-manes.versions")
     repositories { mavenCentral() }
+
+    fun isNonStable(version: String): Boolean {
+        val stableKeyword =
+            listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+        val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+        val isStable = stableKeyword || regex.matches(version)
+        return isStable.not()
+    }
+    tasks.withType<DependencyUpdatesTask> { rejectVersionIf { isNonStable(candidate.version) } }
 }
 
 subprojects {
